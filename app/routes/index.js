8a74edf20b08c9af52cd4713c332eabd58dbd844
Dmitri Grecov <vokerg@gmail.com>
2018-01-14 13:42:57 +0100

    handling service edit

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,7 +1,9 @@
 const shopsRoutes = require('./shops_routes');
 const ratingsRoutes = require('./ratings_routes');
+const servicesRoutes = require('./services_routes')
 
 module.exports = (app, db) => {
   shopsRoutes(app, db);
   ratingsRoutes(app, db);
+  servicesRoutes(app, db);
 };
---
+++ app/routes/services_routes.js
@@ -0,0 +1,9 @@
+var {ObjectId} = require('mongodb');
+
+module.exports = (app, db) => {
+  app.get('/services/', (req, res) => {
+    db.collection("shops").distinct("services", (err, docs) => {
+      res.send(docs)
+    })
+  });
+}
