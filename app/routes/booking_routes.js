264fbbbd0a33b81e39a43a848ff67637dcdc6c7a
Dmitri Grecov <vokerg@gmail.com>
2018-02-17 18:47:34 +0100

    aggregate bookings

--- app/routes/booking_routes.js
+++ app/routes/booking_routes.js
@@ -1,4 +1,4 @@
-const {ObjectId} = require('mongodb');
+const { ObjectId } = require('mongodb');
 const authenticate = require('../passport/authenticate');
 
 module.exports = (app, db) => {
@@ -8,9 +8,65 @@ module.exports = (app, db) => {
         ...req.body,
         shopId: req.params.id
       };
-      db.collection("bookings").insert(booking, (err, result) => {
+      db.collection("bookings").insert({
+        ...booking,
+        userId: new ObjectId(booking.userId),
+        shopId: new ObjectId(booking.shopId)
+      }, (err, result) => {
         res.send(result)
       });
     });
+  });
+
+  app.get('/shops/:id/bookings', (req, res) => {
+    //authenticate(req, res, () => {
+      db.collection("bookings").aggregate(
+        [
+          {
+            $lookup:
+              {
+                from: "shops",
+                localField: "shopId",
+                foreignField: "_id",
+                as: "shops_docs"
+              }
+          }
+          ,{
+            $unwind: "$shops_docs"
+          },
+          {
+            $lookup:
+              {
+                from: "users",
+                localField: "userId",
+                foreignField: "_id",
+                as: "users_docs"
+              }
+          },
+          {
+            $unwind: "$users_docs"
+          }
+          ,{
+            $project:
+              {
+                "_id": 1,
+                "userId": 1,
+                "shopId": 1,
+                "shopname": "$shops_docs.name",
+                "username": "$users_docs.username"
+              }
+          },
+          {
+            $match:
+              {
+                shopId: new ObjectId("5a5b6afbcea79c26d490f533")
+              }
+          }
+        ]
+      )
+        .toArray((err, bookings) => {
+        res.send(bookings);
+      })
+    //})
   });
 };
