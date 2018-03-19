5a9b59ea74cff0403a127d52284d1f734d2310ea
Dmitri Grecov <vokerg@gmail.com>
2018-03-19 12:42:45 +0100

    fixing bugs

--- app/routes/user_routes.js
+++ app/routes/user_routes.js
@@ -1,4 +1,5 @@
-var {ObjectId} = require('mongodb');
+const {ObjectId} = require('mongodb');
+const authenticate = require('../passport/authenticate');
 
 const addFavorites = (user, shopId) => {
   let favorites = user.favorites === undefined ? [] : user.favorites;
@@ -10,33 +11,37 @@ const addFavorites = (user, shopId) => {
 }
 
 module.exports = (app, db) => {
-  
+
   app.put('/users/:id/favorites/:shopId', (req, res) => {
-    const {shopId, id} = req.params;
-    const shopObjectId = {_id: new ObjectId(shopId)};
-    const userObjectId = {_id: new ObjectId(id)};
-    db.collection("users").findOne({...userObjectId}, (err, user) => {
-      if (user === null) {
-        return res.status(400).send(false);
-      }
-      db.collection("shops").findOne({...shopObjectId}, (err, shop) => {
-        if (shop === null) {
+    authenticate(req, res, () => {
+      const {shopId, id} = req.params;
+      const shopObjectId = {_id: new ObjectId(shopId)};
+      const userObjectId = {_id: new ObjectId(id)};
+      db.collection("users").findOne({...userObjectId}, (err, user) => {
+        if (user === null) {
           return res.status(400).send(false);
         }
-        db.collection("users").update(userObjectId, addFavorites(user, shopId), (err, result) => {
-          res.status(200).send(true);
+        db.collection("shops").findOne({...shopObjectId}, (err, shop) => {
+          if (shop === null) {
+            return res.status(400).send(false);
+          }
+          db.collection("users").update(userObjectId, addFavorites(user, shopId), (err, result) => {
+            res.status(200).send(true);
+          });
         });
       });
     });
   });
 
   app.get('/users/:id/favorites', (req, res) => {
-    const userObjectId = {_id: new ObjectId(req.params.id)};
-    db.collection("users").findOne({...userObjectId}, (err, user) => {
-      if (user === null) {
-        return res.status(400).send(false);
-      }
-      res.status(200).send(user.favorites === undefined ? [] : user.favorites);
+    authenticate(req, res, () => {
+      const userObjectId = {_id: new ObjectId(req.params.id)};
+      db.collection("users").findOne({...userObjectId}, (err, user) => {
+        if (user === null) {
+          return res.status(400).send(false);
+        }
+        res.status(200).send(user.favorites === undefined ? [] : user.favorites);
+      });
     });
-  })
+  });
 }
