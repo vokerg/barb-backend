cd7aadb30841a700a3f79a2d59f6c2cd66a922de
Dmitri Grecov <vokerg@gmail.com>
2018-04-18 10:53:58 +0200

    fixing ratings

--- app/routes/ratings_routes.js
+++ app/routes/ratings_routes.js
@@ -1,39 +1,38 @@
 var {ObjectId} = require('mongodb');
 const authenticate = require('../passport/authenticate');
 
-
 module.exports = (app, db) => {
   app.put('/shops/:id/ratings', (req, res) =>
   {
-    authenticate(req, res, () => {
-      const {userId, shopId, author, rating, comment} = req.body;
-      if (userId !== null) {
-        const userId = {
+    const {userId, shopId, author, rating, comment} = req.body;
+    if (userId) {
+      authenticate(req, res, () => {
+        dbUserId = {
           _id: new ObjectId(userId)
         };
-        db.collection("users").findOne(userId, (err, user) => {
-          const ratingInstance = {author: user.username, shopId, rating, comment};
-          addRating(db, ratingInstance, dbRatingInstance => res.send(dbRatingInstance));
-        })
-      } else {
-        const ratingInstance = {author, shopId, rating, comment};
-        addRating(db, ratingInstance, dbRatingInstance => res.send(dbRatingInstance));
-      }
-    });
+        db.collection("users").findOne(dbUserId, (err, user) => {
+          const ratingInstance = {author: user.username, shopId, rating, comment, userId};
+          addRating(db, ratingInstance, shop => res.send({newAuthor: user.username, shop}));
+        });
+      });
+    } else {
+      const ratingInstance = {author, shopId, rating, comment, userId:null};
+      addRating(db, ratingInstance, shop => res.send({newAuthor: author, shop}));
+    }
   });
 };
 
 addRating = (db, ratingInstance, next) => {
-  const {shopId, author, comment, rating} = ratingInstance;
+  const {shopId, author, comment, rating, userId} = ratingInstance;
   const id = {
-    _id: new ObjectId(req.params.id)
+    _id: new ObjectId(shopId)
   };
   db.collection("shops").findOne(id, (err, shop) => {
     shop = {
       ...shop,
       ratings:[
         ...shop.ratings,
-        {author, comment, rating}
+        {author, comment, rating, userId}
       ]
     };
     db.collection("shops").update(id, shop, (err, result) => {
